ddc6b0300ceffdb0d950cf3315da6247c045acb9
Pratik Bedre <58264799+pratikb2805@users.noreply.github.com>
2020-07-07 18:46:56 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Chatter',
+    version='0.1dev',
+    packages=['pywit',, 'pyowm', 'django'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
